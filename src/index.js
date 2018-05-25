69567dcaa7ac86d2e71919b90b216f9616be301b
Vito Belgiorno-Zegna <bz.vito@gmail.com>
2018-05-25 15:49:56 +1000

    Final output

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 const recipes = require('./recipes');
 const {
 	inquire,
@@ -8,4 +10,5 @@ module.exports = async () => {
 	const { menu = [] } = await inquire();
 	menu.unshift('base');
 	for (let dish of menu) await makeDish(recipes[dish]);
+	console.log(chalk.black.bgGreen('\n~~~~~~~~~~~~~~~~~~~~~\nYou\'ve left The Shed.\n~~~~~~~~~~~~~~~~~~~~~\n'));
 };
